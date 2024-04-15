1abdcdc3a5791c2629136c619c087f1bb5549c78
Taha <tahapasha121@gmail.com>
2024-04-15 22:38:35 +0530

    tailwind configured

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+
+export default function App() {
+    return (
+      <div className="bg-black w-screen h-screen">
+      <Navbar/>
+      </div>
+    );
+  }
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-white text-black p-6 flex justify-between'>
+        <div>Nigga</div>
+        <div className='flex gap-12'>
+            <div>Home</div>
+            <div>About</div>
+            <div>Info</div>
+            <div>Sign Up</div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
