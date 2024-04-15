4f3de8aaef595c72052f98e064787c5d3d0e10bb
Taha <tahapasha121@gmail.com>
2024-04-16 00:14:24 +0530

    made the nav bar change the icons plz

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
+import Footer from "./footer";
 import Home from "./home";
 export default function App() {
     return (
       <div className="">
       <Navbar/>
       <Home/>
+      <Footer/>
       </div>
     );
   }
