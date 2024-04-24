ceb6a78412ef080bd67ad640c92b2ab66f552eb7
Taha <tahapasha121@gmail.com>
2024-04-25 00:17:36 +0530

    set thw width

--- src/components/About_us.js
+++ src/components/About_us.js
@@ -9,8 +9,8 @@ function AboutUs() {
         <div className='flex gap-12 justify-center flex-wrap mt-14 '>
             <div className='sm:w-5/12 sm:h-[440px] bg-pink-300 rounded-3xl w-[280px] h-[320px] bc'><div className='flex justify-center mt-4'><img src={roxky} className='w-28 sm:w-40'></img></div>
             <div className='sm:text-md text-black mt-4 mx-4 para'>The Rotaract Club of MediKardia is a dynamic force in the realm of Rotaract and healthcare, fueled by 162 passionate medical students dedicated to service and excellence. Through a series of medico-exclusive health-related events, the club brings vital healthcare services and information to communities in need. From organizing PHC lead surveys to conducting blood donation drives and health awareness campaigns, the club leaves an indelible mark on society and its very own district, Rotaract 3191. The motto ‘Service over Self’ is striven for in ways unimaginable.</div></div>
-            <div className='sm:w-5/12 sm:h-[440px] bg-pink-300 rounded-3xl w-[280px] h-[430px] bc'><div className='flex justify-center mt-4'><img src={vini} className='w-28 sm:w-40'></img></div>
-            <div className='sm:text-md text-black mt-2 mx-4 para'>
+            <div className='sm:w-5/12 sm:h-[440px] bg-pink-300 rounded-3xl w-[280px] h-[430px] bc'><div className='flex justify-center mt-4'><img src={vini} className='w-24 sm:w-32'></img></div>
+            <div className='sm:text-md text-black mt-1 mx-4 para'>
             We, at Vinimaya, were known as just the literature club of University of Visvesvaraya College of Engineering, UVCE for a long time. Evolving in countless aspects since and Vinimaya now encompasses a much larger diaspora deemed to be the Pop Culture Club of UVCE, while staying true to our roots. We have organised numerous intra college fests such as Escapade (the first edition of a 120 student escape room event), Smart Charades (a spicy twist on the classic Dumb Charades) and YES OFFENCE
 We also cater to the Literature enthusiasts of UVCE by conducting Literature & Lattes.
 
